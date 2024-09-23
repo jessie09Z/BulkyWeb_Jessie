d5e3621c2f95f559b5dec33e09f67177985e161a
Jessie <jessay0901@gmail.com>
2024-09-23 20:43:53 +1000

    update gh-pages content

---
+++ bin/Debug/net8.0/publish/wwwroot/js/product.js
@@ -0,0 +1,62 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+   dataTable= $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/product/getall",
+            "type": "GET",
+            "datatype": "json",
+            "dataSrc": function (json) {
+                // 如果返回的数据没有data属性，可以在这里添加
+                if (!json.hasOwnProperty('data')) {
+                    return json;
+                }
+                return json.data;
+            }
+        },
+        "columns": [
+            { "data": "title", "width": "20%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "listPrice", "width": "10%" },
+            { "data": "author", "width": "15%" },
+          
+            { "data": "category.name", "width": "15%" },
+            { "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a  onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+
+                }
+            , "width": "25%" },
+        ]
+    });
+}
+
+function Delete(url) {
+    
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    });
+}
